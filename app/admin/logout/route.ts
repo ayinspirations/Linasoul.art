e9935ca35e5be5c2850b6aba8a8c9679210b129d
ayinspirations <akanyueksel@outlook.com>
2025-08-10 16:50:15 +0200

    Delete app/admin/logout/route.ts

--- app/admin/logout/route.ts
+++
@@ -1,9 +0,0 @@
-// app/admin/logout/route.ts
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-
-export async function POST() {
-  cookies().delete("admin_auth");
-  return NextResponse.json({ ok: true });
-}
-
