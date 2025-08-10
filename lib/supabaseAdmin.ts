944aa5997012f5586c947c38c07f69f61a63db1e
ayinspirations <akanyueksel@outlook.com>
2025-08-10 18:04:56 +0200

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js"
 
 const URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const SERVICE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
+const SERVICE_KEY = process.env.SUPABASE_SERVICE_ROLE
 
 if (!URL) throw new Error("NEXT_PUBLIC_SUPABASE_URL not set")
-if (!SERVICE_KEY) throw new Error("SUPABASE_SERVICE_ROLE_KEY not set")
+if (!SERVICE_KEY) throw new Error("SUPABASE_SERVICE_ROLE not set")
 
 export const supabaseAdmin = createClient(URL, SERVICE_KEY, {
   auth: { persistSession: false, autoRefreshToken: false },
