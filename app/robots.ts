81b0af8c863831f1a8776c740a07b5112a4d32d6
ayinspirations <akanyueksel@outlook.com>
2025-09-01 12:37:50 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,7 @@
 // app/robots.ts
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: { userAgent: "*", allow: "/" },
     sitemap: "https://linasoul.art/sitemap.xml",
     host: "https://linasoul.art",
   }
