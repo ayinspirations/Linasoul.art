c4b4681b87cd46ae5d8460af7333f1c3b4c9d3d0
ayinspirations <akanyueksel@outlook.com>
2025-09-01 09:52:39 +0200

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,7 +27,7 @@ export default function NavBar() {
     <nav className="fixed top-0 z-50 w-full border-b border-taupe-100 bg-white/80 backdrop-blur-md">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
         {/* Logo */}
-        <Link href="/" className="inline-flex items-center">
+        <Link href="/" className="inline-flex items-center mt-2">
           <Image src="/images/Logo_schwarz_2.png" alt="Linasoul Logo" width={120} height={40} priority />
         </Link>
 
